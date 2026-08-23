4fb7a7586a85e02240fb24e89b7b27fef12e97a7
pripezduk <slavaananev74@gmail.com>
2026-08-23 21:35:21 +0700

    Update nixosstal.sh

--- nixosstal.sh
+++ nixosstal.sh
@@ -7,7 +7,7 @@ sed -i '5d' /etc/nixos/configuration.nix
 sed -i '6d' /etc/nixos/configuration.nix
 sed -i '7d' /etc/nixos/configuration.nix
 curl -s -o /tmp/temp.txt https://raw.githubusercontent.com/prizduk/vxwm-on-NixOS/refs/heads/main/vstavka.txt
-printf '0r /tmp/temp.txt\nw\nq' | ed -s /etc/nixos/configuration.nix
+printf '0r /tmp/temp.txt\nw\nq' | nix-shell -p ed --run  'ed -s /etc/nixos/configuration.nix'
 rm /tmp/temp.txt
 awk -v r="$(cat /tmp/hash.txt)" '{gsub(/"00000000000000000000000000000000";/, r)}1' /etc/nixos/configuration.nix > tmp && mv tmp /etc/nixos/configuration.nix
 rm /tmp/hash.txt
